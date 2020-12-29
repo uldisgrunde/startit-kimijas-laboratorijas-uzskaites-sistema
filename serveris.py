37a562f5b91ce81101de6c762e6f39088ba9ea2a
Uldis Grunde <uldis.grunde@gmail.com>
2020-12-29 15:48:23 +0000

    modi add ,del

--- serveris.py
+++ serveris.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, json, jsonify, render_template
 import dati
 
 
@@ -40,19 +40,62 @@ def lietotajs():
 
 @app.route('/api/v1/vielas')
 def vielas():
+    with open("dati/vielas.json", "r", encoding="utf-8") as f:
+      dati=json.loads(f.read())
     # pārveidojam par json pirms atgriežam
-    return jsonify(dati.vielas)
+    return jsonify(dati)
+
+@app.route('/api/v1/viela', methods = ['POST'])
+def add_vielas():
+  datne = "dati/vielas.json"
+  with open(datne, "r", encoding="utf-8") as f:
+    vielas=json.loads(f.read())
+    #print(len(vielas))
+  last_id=vielas[len(vielas)-1]['id']
+  jauna_viela=json.loads(request.data)
+  jauna_viela['id']=last_id+1
+  viela.append(jauna_viela)
+  with open(datne, "w", encoding = "utf-8") as f:
+    f.write(json.dumps(new_data))
+    # pārveidojam par json pirms atgriežam
+  return "1"
+
+@app.route('/api/v1/<kategorija>/<id>/dzest', methods = ['POST'])
+def dz_vielas(kategorija,id):
+  if kategorija == "viela":
+    datne = "dati/vielas.json"
+  elif kategorija == "inventars":
+    datne = "dati/inventars.json"
+  else:
+    return "0"
+
+  with open(datne, "r", encoding = 'utf-8') as f:
+    dati = json.loads(f.read())
+
+  new_data = []
+  for v in dati:
+    if str(v['id']) != id:
+      new_data.append(v)
+
+  with open(datne, "w", encoding = "utf-8") as f:
+    f.write(json.dumps(new_data))
+
+  return "1"
 
 
-@app.route('/api/v1/viela/<vielasID>')
+
+@app.route('/api/v1/viela/<vielasID>', methods=['GET'])
 def viela_id(vielasID):
     # Noklusēta vērtība, ja viela netiks atrasta
     viela = "Viela ar ID {} neeksistē".format(vielasID)
     # meklējam vielu sarakstā
-    for v in dati.vielas:
-        # vielas ID ir skaitlis, jāpārveido datu tips
-        if v["id"] == int(vielasID):
-            viela = v
+    with open("dati/vielas.json", "r", encoding="utf-8") as f:
+      dati=json.loads(f.read())
+        
+    for v in dati:
+      # vielas ID ir skaitlis, jāpārveido datu tips
+      if v["id"] == int(vielasID):
+        viela = v
     return jsonify(viela)
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -6,25 +6,25 @@ async function raditNoliktavasDatus(tipsAtlase)
 
   if(tipsAtlase==undefined)
   {
-    let vielasNoServera = await fetch('https://pytonc.eu.pythonanywhere.com/api/v1/vielas');
+    let vielasNoServera = await fetch('/api/v1/vielas');
     jsonVielas = await vielasNoServera.json();
     jsonVielas = pievienotIerakstuParKategoriju(jsonVielas, 'viela');
     //
-    let inventarsNoServera = await fetch('https://pytonc.eu.pythonanywhere.com/api/v1/inventars');
+    let inventarsNoServera = await fetch('/api/v1/inventars');
     jsonInventars = await inventarsNoServera.json();
     jsonInventars = pievienotIerakstuParKategoriju(jsonInventars, 'inventars');
   }
   else if(tipsAtlase=='viela')
   {
     //jsonVielas = await iegutDatusNoApi('https://pytonc.eu.pythonanywhere.com/api/v1/vielas');
-    let vielasNoServera = await fetch('https://pytonc.eu.pythonanywhere.com/api/v1/vielas');
+    let vielasNoServera = await fetch('/api/v1/vielas');
     jsonVielas = await vielasNoServera.json();
     jsonVielas = pievienotIerakstuParKategoriju(jsonVielas, 'viela');
   }
   else if(tipsAtlase=='aprikojums')
   {
     //jsonInventars = await iegutDatusNoApi('https://pytonc.eu.pythonanywhere.com/api/v1/inventars');
-    let inventarsNoServera = await fetch('https://pytonc.eu.pythonanywhere.com/api/v1/inventars');
+    let inventarsNoServera = await fetch('/api/v1/inventars');
     jsonInventars = await inventarsNoServera.json();
     jsonInventars = pievienotIerakstuParKategoriju(jsonInventars, 'inventars');
   }
@@ -40,43 +40,25 @@ async function raditNoliktavasDatus(tipsAtlase)
   //datiNoliktava['0'] - pirmais ieraksts
   //datiNoliktava['0']['Nosaukums'] - pirmā ieraksta nosaukums
   
-  for (i = 0; i < datiNoliktava.length; i++)
-  {
-
+  for (i = 0; i < datiNoliktava.length; i++)  {
       kategorija = datiNoliktava[i]['kategorija'];
-
-
-      tabula.innerHTML = tabula.innerHTML+`
-      <tr id="`+kategorija+datiNoliktava[i]['id']+`">
-      <td> `+datiNoliktava[i]['id']+` </td>
-      <td> `+datiNoliktava[i]['nosaukums']+` </td>
-      <td> `+datiNoliktava[i]['tips']+` </td>
-      <td> `+datiNoliktava[i]['apakstips']+` </td>
-      <td> `+datiNoliktava[i]['skaits']+` </td>
-      <td> `+datiNoliktava[i]['daudzums']+` </td>
-      <td> `+datiNoliktava[i]['komentari']+` </td>
-      <td onclick="dzestVieluAprikojumu(`+datiNoliktava[i]['id']+`,'`+kategorija+`')"> ❌ </td>
+      tabula.innerHTML += `
+      <tr id="${kategorija+datiNoliktava[i]['id']}">
+      <td>${datiNoliktava[i]['id']}</td>
+      <td>${datiNoliktava[i]['nosaukums']}</td>
+      <td>${datiNoliktava[i]['tips']}</td>
+      <td>${datiNoliktava[i]['apakstips']}</td>
+      <td>${datiNoliktava[i]['skaits']}</td>
+      <td>${datiNoliktava[i]['daudzums']}</td>
+      td>${datiNoliktava[i]['komentari']} </td>
+      <td onclick="dzestVieluAprikojumu('${datiNoliktava[i]['id']}','${kategorija}')"> ❌ </td>
       </tr>`;
-      
-
-
+ 
   }//loop beigas
-  
+  }//beidzas raditNoliktavasDatus(dati)
 
 
-}//beidzas raditNoliktavasDatus(dati)
-
-
-
-
-
-
-
-
-
-async function pievienotVieluAprikojumu()
-{
-
+async function pievienotVieluAprikojumu(){
 let requestBodyJson = {
               "apakstips": document.querySelector('#apakstips').value,
               "daudzums": parseInt(document.querySelector('#daudzums').value),
@@ -107,12 +89,6 @@ document.querySelector('#atbilde').innerHTML=JSON.stringify(atbilde);
   
 }//beigas pievienotVieluAprikojumu();
 
-
-
-
-
-
-
 function pievienotIerakstuParKategoriju(json, tips)
 {
   for (i = 0; i < json.length; i++)
@@ -123,19 +99,12 @@ function pievienotIerakstuParKategoriju(json, tips)
   return json;
 }
 
-
-
-
-
-
-
 async function dzestVieluAprikojumu(id,kategorija)
 {
-
   
   if (confirm('Dzēst?')) 
   {
-    let request = await fetch('https://pytonc.eu.pythonanywhere.com/api/v1/'+kategorija+'/'+id+'/dzest',
+    let request = await fetch('/api/v1/'+kategorija+'/'+id+'/dzest',
                 {
                 method:"POST",
                 headers: {
