88c1c80fa58db9fc79668569dfc5388e6c7a0ccb
Uldis Grunde <uldis.grunde@gmail.com>
2020-12-29 09:20:37 +0000

    pieliku /sveiki

--- serveris.py
+++ serveris.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Labrīt!"
-
+    return "Sveiks!"
+@app.route('/sveiki')
+def sveiki():
+    return "Nav nekāds rīts!"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
