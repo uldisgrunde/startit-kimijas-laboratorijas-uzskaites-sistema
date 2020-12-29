80dbb65cc41af0a75dfad9116a35e460ad2b85e6
Uldis Grunde <uldis.grunde@gmail.com>
2020-12-29 09:44:32 +0000

    root cik

--- serveris.py
+++ serveris.py
@@ -10,6 +10,16 @@ def index():
 @app.route('/sveiki')
 def sveiki():
     return "Nav nekāds rīts!"
+@app.route('/sveiki/<vards>')
+def sveiki_vards(vards):
+    #return "Sveiki {}".format(vards) 
+    return f"<h1>Sveiks {vards}!</h1>"
+@app.route('/cik/<sk1>/<sk2>')
+def reizinajums(sk1,sk2):
+    return str(int(sk1)*int(sk2))
+
+
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
